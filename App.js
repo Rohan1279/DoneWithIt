e8571ccc382d3d764619c197f28055c1488d6a0a
Rohan1279 <muniruddinrohan@gmail.com>
2023-08-08 10:44:13 +0600

    add end code for navigation

--- App.js
+++ App.js
@@ -1,41 +1,13 @@
 import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-import Screen from "./app/components/Screen";
-import { Button, Text } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
-const Link = ({}) => {
-  const navigation = useNavigation();
-  return (
-    <Button
-      title="Click"
-      onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
-    />
-  );
-};
-const Tweets = ({ navigation }) => (
-  <Screen>
-    <Text>Tweets</Text>
-    <Link />
-  </Screen>
-);
-const TweetDetails = ({ route }) => (
-  <Screen>
-    <Text>Tweet Details {route.params.id}</Text>
-  </Screen>
-);
-const Stack = createStackNavigator();
-const StackNavigator = () => (
-  <Stack.Navigator initialRouteName="">
-    <Stack.Screen name="Tweets" component={Tweets} />
-    <Stack.Screen name="TweetDetails" component={TweetDetails} />
-  </Stack.Navigator>
-);
+import { NavigationContainer } from "@react-navigation/native";
+
+import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <StackNavigator />
+    <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
     </NavigationContainer>
   );
 }
